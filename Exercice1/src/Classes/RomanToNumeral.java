9479871d7bdb3233feabc58d29093e67f8bc0964
Pierre <flaeur@gmail.com>
2019-02-28 13:45:54 +0100

    Implementation de la fonction getNumeral

--- Exercice1/src/Classes/RomanToNumeral.java
+++ Exercice1/src/Classes/RomanToNumeral.java
@@ -2,6 +2,22 @@ package Classes;
 
 public class RomanToNumeral {
     public int getNumeral(char numeral){
+        switch (numeral){
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+        }
         return 0;
     }
 }
