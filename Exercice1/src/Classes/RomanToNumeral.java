957c199871023096e233f22e488731220fa38180
Pierre <flaeur@gmail.com>
2019-02-28 15:11:25 +0100

    implemetation de getNumeral

--- Exercice1/src/Classes/RomanToNumeral.java
+++ Exercice1/src/Classes/RomanToNumeral.java
@@ -1,8 +1,20 @@
 package Classes;
 
 public class RomanToNumeral {
-    public int getNumeral(char roman){
-        return 0;
+    public int getNumeral(String roman){
+        int numeral = 0;
+        for (int i = 0; i<roman.length(); i++){
+            if (getCharNumeral(roman.charAt(i)) > getCharNumeral(roman.charAt(i+1))){
+                numeral += getCharNumeral(roman.charAt(i));
+            }
+            else if (getCharNumeral(roman.charAt(i)) < getCharNumeral(roman.charAt(i+1)) && getCharNumeral(roman.charAt(i)) * 10 < getCharNumeral(roman.charAt(i+1)) && (roman.charAt(i) == 'I' || roman.charAt(i) == 'X' || roman.charAt(i) == 'C')){
+                numeral = numeral + getCharNumeral(roman.charAt(i+1)) - getCharNumeral(roman.charAt(i));
+                i++;
+            }
+            else
+                return 0;
+        }
+        return numeral;
     }
 
     public int getCharNumeral(char roman){
