966a0b35aceaf7d0d2afe635351117aa2a7ecea6
Pierre <flaeur@gmail.com>
2019-02-28 14:49:56 +0100

    Creation de la classe RomanAdditionner

---
+++ Exercice1/src/Classes/RomanAdditionner.java
@@ -0,0 +1,13 @@
+package Classes;
+
+import java.util.ArrayList;
+
+public class RomanAdditionner {
+    public String addition (ArrayList<String> operandes){
+        return "";
+    }
+
+    public String soustraction (ArrayList<String> operandes){
+        return "";
+    }
+}
